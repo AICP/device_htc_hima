def8a7809515e101c04b8eb4e9829ae9343e46cd
Julian Veit <claymore1298@gmail.com>
2018-03-01 20:46:45 +0100

    hima-common: add qcom legacy-sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -148,6 +148,7 @@ BOARD_GLOBAL_CFLAGS += -DCOMPAT_SENSORS_M
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
 # Shims
