22a7ce6409b1f37fe2939d5c04ec3ca6e2422eef
Rashed Abdel-Tawab <rashed@linux.com>
2018-10-11 14:46:37 +0300

    hima-common: Ignore Selinux Neverallows
    Change-Id: I585ae6bac9d0ed303422f8dfa85ecf83932bb2cd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -163,6 +163,11 @@ BOARD_GLOBAL_CFLAGS += -DCOMPAT_SENSORS_M
 include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libHtcVteEngine.so|libshim_camera.so \
