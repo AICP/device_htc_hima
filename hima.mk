2869bfe3a695a203498020de2821710c45aea34f
Ethan Chen <intervigil@gmail.com>
2018-11-02 00:32:56 -0700

    hima-common: Don't use hardware/broadcom configuration
    * We should enable the p2p group interface
    
    Change-Id: Ib4470b0404f75efc4b528c6b9e5c9ffa2f5ab3c8

--- hima.mk
+++ hima.mk
@@ -36,9 +36,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Add WiFi Config files
-$(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
