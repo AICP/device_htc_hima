3ecb7af90ffe2acb4081aac542f2451fbd5bd718
Julian Veit <claymore1298@gmail.com>
2018-11-28 22:25:31 +0100

    hima-common: remove overlay-lineage
    Change-Id: I62814c682798aa0e52c5527ab6d819c746a6da19

--- hima.mk
+++ hima.mk
@@ -21,8 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
