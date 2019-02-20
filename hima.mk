12e8d6c84da771513948a03574edeea33857e0f1
Julian Veit <claymore1298@gmail.com>
2019-02-20 21:35:08 +0100

    Revert "hima-common: Convert power HAL for hima"
    This reverts commit 7de5e80a870043fdbbd92d79c1a474ec86f637de.
    
    Change-Id: I6436ef7f5b8037458173c31f3c15acb73eb15264

--- hima.mk
+++ hima.mk
@@ -265,8 +265,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.qcom
+    android.hardware.power@1.0-service
 
 # QMI
 PRODUCT_PACKAGES += \
