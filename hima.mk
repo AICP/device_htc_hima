1f54867bd6a6ab5efd2eaa85d17a0a67a34afc37
Julian Veit <claymore1298@gmail.com>
2019-07-12 15:21:36 +0200

    Revert "hima-common: build SnapdragonCamera"
    This reverts commit 85933aa1103766ad5d2f359cffe999e9902c34fc.
    
    Change-Id: Iec4a7bf6fe77d002b6816128a69f27fe77063d33

--- hima.mk
+++ hima.mk
@@ -118,7 +118,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-hima \
     libshim_camera_hima \
-    SnapdragonCamera
+    Snap
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hdrhax:$(TARGET_COPY_OUT_VENDOR)/etc/hdrhax
