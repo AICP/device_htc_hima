7354d367f8450eb0d70fe39223c927cacd83412a
Julian Veit <claymore1298@gmail.com>
2019-10-03 21:13:34 +0200

    hima: define default fingerprint in build.prop
    Change-Id: Idc3074ad89e7bf06a6e767a868d00d19cf9f1c7b

--- aicp_hima.mk
+++ aicp_hima.mk
@@ -5,13 +5,6 @@ $(call inherit-product, device/htc/hima/full_hima.mk)
 
 PRODUCT_NAME := aicp_hima
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="htc_himauhl" \
-    PRODUCT_NAME="himauhl_htc_europe" \
-    PRIVATE_BUILD_DESC="4.28.401.3 CL869604 release-keys"
-
-BUILD_FINGERPRINT="htc/himauhl_htc_europe/htc_himauhl:7.0/NRD90M/869604.3:user/release-keys"
-
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DEVICE_MAINTAINERS="Julian Veit (Claymore1297), Ali B (eyosen), Joel Stein (k4y0z)"
