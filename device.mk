1635791136fb61c89ff2c2451946ad158110f9e1
Bruno Martins <bgcngm@gmail.com>
2019-10-04 23:20:42 +0100

    hima: Switch to legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba

--- device.mk
+++ device.mk
@@ -345,7 +345,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     libwifi-hal-bcm \
     libwpa_client \
     hostapd \
