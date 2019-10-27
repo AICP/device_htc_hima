c8b7d729135d3d15c8fa4b17f6c381fcc4d26191
Julian Veit <claymore1298@gmail.com>
2019-10-27 09:02:48 +0100

    Revert "hima: Switch to legacy Wi-Fi service"
    This reverts commit 1635791136fb61c89ff2c2451946ad158110f9e1.
    
    Change-Id: Icb80e92958e1f3607cfe2ced858198d973522b01

--- device.mk
+++ device.mk
@@ -345,7 +345,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi@1.0-service \
     libwifi-hal-bcm \
     libwpa_client \
     hostapd \
