a64b6b013b5465a1aad9b196a2bbd5f2f1772758
Julian Veit <claymore1298@gmail.com>
2020-03-28 19:19:41 +0100

    hima: DeviceParts: introduce headphone gain
    Change-Id: I148848f260f32194aff367c9ea589999a8b036a9

--- DeviceParts/src/com/aicp/device/Startup.java
+++ DeviceParts/src/com/aicp/device/Startup.java
@@ -46,5 +46,6 @@ public class Startup extends BroadcastReceiver {
 
         VibratorStrengthPreference.restore(context);
         BacklightDimmerPreference.restore(context);
+        HeadphoneGainPreference.restore(context);
     }
 }
