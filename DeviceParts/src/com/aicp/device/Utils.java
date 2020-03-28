7e46116f26788575a4d0746d4f651db446167cff
Julian Veit <claymore1298@gmail.com>
2020-03-28 19:19:41 +0100

    hima: DeviceParts: introduce headphone gain
    Change-Id: I148848f260f32194aff367c9ea589999a8b036a9

--- DeviceParts/src/com/aicp/device/Utils.java
+++ DeviceParts/src/com/aicp/device/Utils.java
@@ -58,7 +58,7 @@ public class Utils {
      * @param filename      The filename
      * @param value         The value
      */
-    public static void writeValueiSimple(String filename, String value) {
+    public static void writeValueSimple(String filename, String value) {
         if (filename == null) {
             return;
         }
@@ -67,6 +67,29 @@ public class Utils {
             FileOutputStream fos = new FileOutputStream(new File(filename));
             String valueSimple = value + "\n";
             fos.write(valueSimple.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Write a string value to the specified file.
+     * @param filename      The filename
+     * @param value         The value
+     */
+    public static void writeValueDual(String filename, String value) {
+        if (filename == null) {
+            return;
+        }
+	String Dualvalue = value+" "+value;
+        if (DEBUG) Log.d(TAG, "writeValueDual: filename / value:"+filename+" / "+Dualvalue);
+        try {
+            FileOutputStream fos = new FileOutputStream(new File(filename));
+            fos.write(Dualvalue.getBytes());
+            fos.flush();
             fos.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -128,6 +151,26 @@ public class Utils {
         return declutteredValue;
     }
 
+    /*
+     * we need this little helper method, because api offers us values for left and right.
+     * We want to handle both values equal, so only read left value.
+     * Format in sysfs file is:
+     * 1 1
+     * BUT... for some reasons, when writing in the file a -1, the value in the file is 255,
+     * -2 is 254, so we have here to do some maths...
+    */
+    public static String declutterDualValue(String HtcOutput) {
+        String[] seperateDual = HtcOutput.split(" ", 2);
+        int declutteredValue = Integer.parseUnsignedInt(seperateDual[0]);
+        if (declutteredValue > 20) {
+            // The chosen variablename is like the thing it does ;-) ...
+            int declutteredandConvertedValue = declutteredValue - 256;
+            declutteredValue = declutteredandConvertedValue;
+        }
+        Log.i(TAG,"deplutterDualvalue: decluttered value: "+declutteredValue);
+        return String.valueOf(declutteredValue);
+    }
+
     public static boolean getFileValueAsBoolean(String filename, boolean defValue) {
         String fileValue = readLine(filename);
         if(fileValue!=null){
@@ -140,7 +183,7 @@ public class Utils {
         String fileValue = readLine(filename);
         if (DEBUG) Log.d(TAG,"getFileValue file / value:"+filename+" / "+fileValue);
         if(fileValue!=null){
-	    return fileValue;
+            return fileValue;
         }
         if (DEBUG) Log.e(TAG,"getFileValue file / value:"+filename+" / "+defValue);
         return defValue;
@@ -149,9 +192,19 @@ public class Utils {
     public static String getFileValueVibrator(String filename, String defValue) {
         String fileValue = readLine(filename);
         if(fileValue!=null){
-	    return declutterVibratorValue(fileValue);
+            return declutterVibratorValue(fileValue);
         }
         if (DEBUG) Log.d(TAG,"getFileValue file / value:"+filename+" / "+defValue);
+        return defValue;
+    }
+
+    public static String getFileValueDual(String filename, String defValue) {
+        String fileValue = readLine(filename);
+        if (DEBUG) Log.d(TAG,"getFileValueDual: file / value:"+filename+" / "+fileValue);
+        if(fileValue!=null){
+            return declutterDualValue(fileValue);
+        }
+        if (DEBUG) Log.e(TAG,"getFileValueDual: file / default value:"+filename+" / "+defValue);
         return defValue;
     }
 }
