5cce30e135fd6e2565ea21f09a4314212c67bed0
Julian Veit <claymore1298@gmail.com>
2020-07-05 18:38:38 +0200

    hima: DeviceParts: move to kotlin
    leave out the old gesture-app for now.
    
    Change-Id: Ia0a5c07d956119d88656b39e3134bb6c4ad82a67

---
+++ DeviceParts/src/com/aicp/device/Startup.kt
@@ -0,0 +1,49 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+* Copyright (C) 2020 The Android Ice Cold Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.aicp.device
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+
+class Startup : BroadcastReceiver() {
+    override fun onReceive(context: Context, bootintent: Intent?) {
+        restoreAfterUserSwitch(context)
+        val serviceIntent = Intent(context, HtcGestureService::class.java)
+        context.startService(serviceIntent)
+    }
+
+    companion object {
+        private fun restore(file: String?, enabled: Boolean) {
+            if (file == null) {
+                return
+            }
+            Utils.writeValueSimple(file, if (enabled) "1" else "0")
+        }
+
+        fun restoreAfterUserSwitch(context: Context) {
+            val enabled = Settings.System.getInt(context.getContentResolver(), FastChargeSwitch.Companion.SETTINGS_KEY, 0) != 0
+            restore(FastChargeSwitch.Companion.file, enabled)
+            VibratorStrengthPreference.Companion.restore(context)
+            BacklightDimmerPreference.Companion.restore(context)
+            HeadphoneGainPreference.restore(context)
+        }
+    }
+}
