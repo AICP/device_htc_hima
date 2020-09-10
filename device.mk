fc7e49842992cf32d72ade295d9efc6ea347ce00
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    hima: Build libhidltransport and libhwbinder
    Change-Id: I1b4b255ce40c94e3bf2e6234b7280fc094a9270b

--- device.mk
+++ device.mk
@@ -185,6 +185,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhwbinder
+
 # HTC Logging functions
 PRODUCT_PACKAGES += \
     liblog_htc
