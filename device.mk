b8bde738b8215db9689da67f81df0f9cb887118c
Julian Veit <claymore1298@gmail.com>
2020-10-11 06:10:43 +0200

    hima: build libprotobuf-legacy as prebuilt library
    libprotobuf-legacy is libprotobuf-full from Q
    
    Change-Id: I6bdfbc48e915cf642903438fdddda0e7cc3d16b2

--- device.mk
+++ device.mk
@@ -302,7 +302,8 @@ PRODUCT_PACKAGES += \
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
+    libprotobuf-cpp-lite-vendorcompat \
+    libprotobuf-legacy
 
 # QMI
 PRODUCT_PACKAGES += \
