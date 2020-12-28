9c2c7a136fd8cf7ca032268668ec8ed0316e7487
Julian Veit <claymore1298@gmail.com>
2020-12-28 15:22:15 +0100

    hima: DeviceParts: introduce tuneable for speaker protection
    also move the property into init routine, so there is no misleading
    info in system.prop and add the needed selinux rules.
    
    Change-Id: I0974fcd661fa9fc1dcc1841abe43f0f2d214acf3

---
+++ DeviceParts/src/com/aicp/device/SpeakerProtection.kt
@@ -0,0 +1,57 @@
+/*
+* Copyright (C) 2020 The Android Ice Cold Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.aicp.device
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.provider.Settings
+import android.util.Log
+import androidx.preference.Preference
+import androidx.preference.Preference.OnPreferenceChangeListener
+import androidx.preference.PreferenceManager
+
+class SpeakerProtectionSwitch(context: Context) : OnPreferenceChangeListener {
+    private val mContext: Context
+    override fun onPreferenceChange(preference: Preference?, newValue: Any): Boolean {
+        val enabled = newValue as Boolean
+        Settings.System.putInt(mContext.getContentResolver(), SETTINGS_KEY, if (enabled) 1 else 0)
+        Utils.setSystemProp(SpeakerProtectionSwitch.SYSTEM_PROP, if (enabled) "true" else "false")
+        return true
+    }
+
+    companion object {
+        const val SETTINGS_KEY = DeviceSettings.KEY_SETTINGS_PREFIX + DeviceSettings.KEY_SPEAKER_PROTECTION
+        const val SYSTEM_PROP = "persist.vendor.audio.speaker.prot.enable"
+        fun isCurrentlyEnabled(context: Context?): Boolean {
+            if(Utils.getSystemProp(SYSTEM_PROP).equals("true"))
+            {
+                return true
+            }else {
+                return false
+            }
+        }
+        fun restore(storedValue: String) {
+            Utils.setSystemProp(SYSTEM_PROP,storedValue);
+        }
+
+    }
+
+    init {
+        mContext = context
+    }
+}
