ca50f8296fec557e243ecaf216e39d2a2f0ac3ff
Julian Veit <Claymore1298@gmail.com>
2021-03-07 11:12:23 +0100

    hima: add shim for camera_chromaflash
    03-07 10:34:28.844   666   666 E mm-camera-img: frameproc_comp_reload_lib:1049] Error opening frameproc library libmmcamera_chromaflash_lib.so error dlopen failed: cannot locate symbol "_Z22chromaFlash_icRvJzIm8lbbbbbbbfffjjffffffjfffffiffjjj" referenced by "/vendor/lib/libmmcamera_chromaflash_lib.so"...
    
    Change-Id: Id59ea667aaecfb102739f03d0493238880de12c7

--- device.mk
+++ device.mk
@@ -127,6 +127,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-hima \
     libshim_camera_hima \
+    libshim_chromaflash \
     libshim_stillmore \
     Snap \
     cameraserver_hima
